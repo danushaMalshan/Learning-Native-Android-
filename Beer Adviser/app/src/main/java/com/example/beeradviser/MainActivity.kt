00088058261fab891c24b572eb72b70673b3f810
Danusha Malshan <dan.malshan@gmail.com>
2023-07-21 00:27:31 +0530

    initial commit

---
+++ Beer Adviser/app/src/main/java/com/example/beeradviser/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.beeradviser
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Spinner
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val findBeer=findViewById<Button>(R.id.find_beer)
+
+
+
+
+        findBeer.setOnClickListener {
+            val beerColor= findViewById<Spinner>(R.id.beer_color)
+            val color=beerColor.selectedItem
+
+            val beerList=getBeers(color.toString())
+            val beers=beerList.reduce { str, item ->str+ '\n'+item  }
+
+            val brands=findViewById<TextView>(R.id.brands)
+            brands.text=beers
+
+        }
+    }
+
+
+    fun getBeers(color:String):List<String>{
+        return when (color){
+            "Light"-> listOf("Jail Pale Ale","Lager Lite")
+            "Amber"-> listOf("Jack Amber","Red Brownie")
+            "Brown"-> listOf("Brown Bear Beer","Bock Brownie")
+            else-> listOf("Gout Stout","Dark Daniel")
+        }
+    }
+}
